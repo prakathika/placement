710f10ad2a0ae22ad5960b67452b79da4af56075
prakathika <157699994+prakathika@users.noreply.github.com>
2025-02-24 10:13:36 +0530

    Add files via upload

---
+++ Area (1).java
@@ -0,0 +1,23 @@
+import java.util.*;
+abstract class Circle{
+    protected double area;
+    abstract void calc_area();
+    
+}
+class Area extends Circle{
+    private double r;
+    public Area(){
+      Scanner in=new Scanner(System.in);
+      r=in.nextDouble();
+}
+  void calc_area(){
+    area=3.14*r*r;
+    System.out.println("Area ="+area);
+}
+
+
+	public static void main(String[] args) {
+	      Area obj=new Area();
+	      obj.calc_area();
+	}
+}
